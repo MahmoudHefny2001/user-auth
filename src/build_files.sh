658622c96227ab69cc00c9eb192a0b1699138f61
MahmoudHefny2001 <hefny4@gmail.com>
2024-03-18 16:35:56 +0200

    Message

--- src/build_files.sh
+++ src/build_files.sh
@@ -1,4 +1,12 @@
+echo "Starting build process"
+
 pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
+
+mkdir media 
+mkdir images
+
+
+echo "Build process complete"
