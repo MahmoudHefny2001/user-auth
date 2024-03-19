0103cfb35c193991c7ea97bd960cf0018ba83ece
MahmoudHefny2001 <hefny4@gmail.com>
2024-03-19 12:16:59 +0200

    Message

--- src/settings/production_settings.py
+++ src/settings/production_settings.py
@@ -9,7 +9,7 @@ load_dotenv(".env.production")
 
 SECRET_KEY = os.environ.get("SECRET_KEY", None)
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = list(str(os.environ.get("ALLOWED_HOSTS")).split(", "))
 
