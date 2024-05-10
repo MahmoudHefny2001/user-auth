8eab773951716d0b2418e194a5d95f6816e97e94
MahmoudHefny2001 <hefny4@gmail.com>
2024-05-10 20:07:33 +0300

    Message

--- src/server/settings/production_settings.py
+++ src/server/settings/production_settings.py
@@ -56,14 +56,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
 
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.backends.MediaCloudinaryStorage",
-    },
-#     "staticfiles": {
-#         "BACKEND": "cloudinary_storage.storage.backends.StaticHashedCloudinaryStorage",
-#     },
-}
 
 
 
