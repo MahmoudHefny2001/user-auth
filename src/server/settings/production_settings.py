84fd279fef5807048294ab560ce18852354b9a9d
MahmoudHefny2001 <hefny4@gmail.com>
2024-05-10 19:43:00 +0300

    Message

--- src/server/settings/production_settings.py
+++ src/server/settings/production_settings.py
@@ -43,14 +43,14 @@ CLOUDINARY_STORAGE = {
 }
 
 
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
