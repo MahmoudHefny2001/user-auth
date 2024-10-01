037955cc0ed7c2e4fbfc614434efad08e1e9ac58
MahmoudHefny2001 <hefny4@gmail.com>
2024-10-01 17:22:20 +0300

    Message

--- src/server/settings/production_settings.py
+++ src/server/settings/production_settings.py
@@ -176,5 +176,19 @@ CORS_ALLOW_HEADERS = [
 ]
 
 LOGGING = {
-    
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
 }
